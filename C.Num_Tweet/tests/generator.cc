7255f9a7d34586e8bf0b5718e4b02c4d4cb2f9e6
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-10 10:08:36 +0000

    Implement generator for C.Num_Tweet

--- C.Num_Tweet/tests/generator.cc
+++ C.Num_Tweet/tests/generator.cc
@@ -1,81 +1,58 @@
-#include <iostream>
-#include <utility>
-#include "./testlib.h"
 #include "./constraints.hpp"
+#include "./testlib.h"
 #include <sys/types.h>
 #include <unistd.h>
-#include <cassert>
-
 using namespace std;
 
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
-void output(int N, int M, const vector<int> &a, const string &prefix, int suffix){
-    char name[100];
-    sprintf(name, "%s_%02d.in", prefix.c_str(), suffix);
-    ofstream ofs(name);
-    ofs << N << ' ' << M << endl;
-    for (int i = 0; i < N; ++i) {
-        ofs << a[i] << (i + 1 == N ? '\n' : ' ');
-    }
-    ofs.close();
+void output(const int &N, const int &K, const vector<int> &A, const string &prefix, const int &num = 0) {
+  ostringstream os;
+  os << prefix << '_' << num << ".in";
+  ofstream ofs(os.str());
+  ofs << N << ' ' << K << '\n';
+  for (auto &x : A) ofs << x << '\n';
+  ofs.close();
 }
 
-vector<int> gen(int N) {
+void case_50_random() {
+  rep(num, 10) {
+    int N = rnd.next(MIN_N, MAX_N);
+    int K = rnd.next(MIN_K, MAX_N);
     vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        A[i] = rnd.next(MIN_A, MAX_A);
-    }
-    return A;
+    rep(i, N) A[i] = rnd.next(MIN_A, i);
+    output(N, K, A, "50_random", num);
+  }
 }
 
-int main(){
-    // 乱数のシードを設定
-    // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
-    rnd.setSeed(time(0)+getpid());
+void case_60_random() { // larger N
+  rep(num, 10) {
+    int N = rnd.next(MAX_N / 2, MAX_N);
+    int K = rnd.next(MIN_K, MAX_K);
+    vector<int> A(N);
+    rep(i, N) A[i] = rnd.next(MIN_A, i);
+    output(N, K, A, "60_random", num);
+  }
+}
 
-    // 乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, MAX_N);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "50_random", i);
-    }
+void case_70_corner() { // stack overflow attack (anti dfs)
+  int N = MAX_N, K = MAX_K;
+  vector<int> A(N);
+  rep(i, N) A[i] = i;
+  output(N, K, A, "70_corner");
+}
 
-    // 色の数Nが少ない乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, 10);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "51_random_Nsmall", i);
-    }
+void case_80_corner() { // single node
+  int N = 1, K = MIN_K;
+  vector<int> A(N);
+  A[0] = 0;
+  output(N, K, A, "80_corner");
+}
 
-    // 最大( LCM(N,M)が大きく、数の範囲の総和も大きい )ケース
-    vector<int> primenums;
-    const auto isPrime = [](int n){
-        if(n<=1)return false;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    };
-    for(int i=900;i<=1000;i++){
-        if(isPrime(i)){
-            primenums.push_back(i);
-        }
-    }
-
-    for(int i = 0; i < 10; ++i){
-        int pos = rnd.next(1,(int)primenums.size()-1);
-        int N = primenums[ pos ];
-        int M = primenums[ rnd.next(0,pos-1) ];
-        assert(M<=N);
-        vector<int> A(N);
-        for(int j=0;j<N;j++){
-            if(j%2==0)A[j]=0;
-            else A[j]=100;
-        }
-        output(N, M, A, "52_NMprime", i);
-    }
-
+int main() {
+  rnd.setSeed(time(0) + getpid());
+  case_50_random();
+  case_60_random();
+  case_70_corner();
+  case_80_corner();
 }
