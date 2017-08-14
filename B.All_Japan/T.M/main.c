83d98f6ab4c2b69048994a8bc3860ea3919b6dd3
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-08-14 20:35:56 +0900

    全日本のT.Mの回答を修正

--- B.All_Japan/T.M/main.c
+++ B.All_Japan/T.M/main.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  int n,f=0,a,b=0,t,d,s=0,i;
-  scanf("%d",&n);
-  for(i=0;n--;i++){
-    scanf("%d %d",&a,&t);
-    d=abs(f-a);
-    if(d>t-b)goto END;
-    if(f+a<=t-b){
-      s+=i*f;
+  int n,d,s=0,i,j,l,a[100010],t[100010];
+  scanf("%d %d",&n,&l);
+  for(i=0;i<n;i++)scanf("%d %d",&a[i+1],&t[i+1]);
+  for(i=j=0;j++<n;i++){
+    if(i>=l)goto END;
+    d=abs(a[j-1]-a[j]);
+    if(d>t[j]-t[j-1])goto END;
+    if(a[j-1]+a[j]<=t[j]-t[j-1]){
+      s+=i*a[j-1];
       i=0;
     }
     else s+=i*d;
-    f=a;
-    b=t;//printf("%d %d\n",s,i+1);
   }
-  printf("%d\n",s+(i*f));
+  printf("%d\n",s+(i*a[j-1]));
   return 0;
  END:;
   printf("-1\n");
