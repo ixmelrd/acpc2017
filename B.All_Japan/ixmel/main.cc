71efb4aac3894516149ad36b83d8f4c835c8c5a7
ixmel <kazuyakazuya524@gmail.com>
2017-08-14 21:56:57 +0900

    ジャッジ解修正

--- B.All_Japan/ixmel/main.cc
+++ B.All_Japan/ixmel/main.cc
@@ -7,12 +7,15 @@ const ll inf=1e8;
 int main(){
 	int n,d;
 	cin>>n>>d;
-	vector<ll>t(n+1,inf),f(n+1,1);
-	rep(i,n)cin>>t[i]>>f[i];
+	vector<ll>t(n+2,inf),f(n+2,1);
+	rep(i,n)cin>>t[i+1]>>f[i+1];
+	t[0]=0;
 	ll out=0,l=0;
-	rep(i,n)if(f[i+1]+f[i]-2<=t[i+1]-t[i])if(d<=i-l){
-		cout<<-1<<endl;
-		return 0;
-	}else while(l!=i+1)out+=t[i]+f[i]-1-t[l++];
+	bool h=true;
+	rep(i,n+1)if(f[i+1]+f[i]-2<=t[i+1]-t[i])if(d<=i-l)h=false;
+	else while(l!=i+1)out+=t[i]+f[i]-1-t[l++];
+	else if(abs(f[i+1]-f[i])>t[i+1]-t[i])h=false;
+
+	if(!h)out=-1;
 	cout<<out<<endl;
 }
