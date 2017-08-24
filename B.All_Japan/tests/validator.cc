2b01b0abfe67c7421b0f05b447dae6738e398df5
yebityon <yebi@yebityonnoMacBook-Air.local>
2017-08-24 15:16:44 +0900

    改行を読み取るように修正

--- B.All_Japan/tests/validator.cc
+++ B.All_Japan/tests/validator.cc
@@ -6,6 +6,7 @@ int main(){
 	registerValidation();
 	int N_CNT=inf.readint(MIN_N,MAX_N);
 	int D_CNT=inf.readint(MIN_D,MAX_D);
+	inf.readEoln();
 	int pret=1;
 	for(int i=0; i<N_CNT;i++){
 		pret=inf.readint(pret+1,MAX_T);
