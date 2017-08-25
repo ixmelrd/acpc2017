655218f92fe017ce03581f4c08ca7828b83d4c3a
yebityon <yebi@yebityonnoMacBook-Air.local>
2017-08-25 16:30:44 +0900

    generatorを追加

--- A.Shuttle_Run/tests/generator.cc
+++ A.Shuttle_Run/tests/generator.cc
@@ -5,77 +5,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <cassert>
-
+#define TEST_CASE 15
+#define rangemax 1e9
+#define rangemin 1
 using namespace std;
-
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
-
-void output(int N, int M, const vector<int> &a, const string &prefix, int suffix){
-    char name[100];
-    sprintf(name, "%s_%02d.in", prefix.c_str(), suffix);
+// oが出る確率が90%  xが出る確率が10%に設定
+void output(string s,int length,const string &name){
     ofstream ofs(name);
-    ofs << N << ' ' << M << endl;
-    for (int i = 0; i < N; ++i) {
-        ofs << a[i] << (i + 1 == N ? '\n' : ' ');
+    ofs << length << endl;
+    rep(i,s.size()){
+	    ofs << s[i];
     }
+    ofs << endl;
     ofs.close();
 }
-
-vector<int> gen(int N) {
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        A[i] = rnd.next(MIN_A, MAX_A);
-    }
-    return A;
-}
-
 int main(){
-    // 乱数のシードを設定
-    // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
-    rnd.setSeed(time(0)+getpid());
-
-    // 乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, MAX_N);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "50_random", i);
-    }
-
-    // 色の数Nが少ない乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, 10);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "51_random_Nsmall", i);
-    }
-
-    // 最大( LCM(N,M)が大きく、数の範囲の総和も大きい )ケース
-    vector<int> primenums;
-    const auto isPrime = [](int n){
-        if(n<=1)return false;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    };
-    for(int i=900;i<=1000;i++){
-        if(isPrime(i)){
-            primenums.push_back(i);
-        }
-    }
-
-    for(int i = 0; i < 10; ++i){
-        int pos = rnd.next(1,(int)primenums.size()-1);
-        int N = primenums[ pos ];
-        int M = primenums[ rnd.next(0,pos-1) ];
-        assert(M<=N);
-        vector<int> A(N);
-        for(int j=0;j<N;j++){
-            if(j%2==0)A[j]=0;
-            else A[j]=100;
-        }
-        output(N, M, A, "52_NMprime", i);
-    }
-
+	rnd.setSeed(time(0)+getpid());
+	rep(case_num,TEST_CASE){
+		char file_name[100];
+		sprintf(file_name,"10-random-%02d.in",case_num);
+		int length=rnd.next(MIN_LENGTH,MAX_LENGTH);
+		string s;
+		rep(i,length){
+			int num=rnd.next(rangemin,rangemax);
+			if(num%10)s.push_back('o');
+			else s.push_back('x');
+		}
+		output(s,length,file_name);
+	}
 }
