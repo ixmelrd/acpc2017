5aca76ccd248d575e3c7a11277064283d045026c
野井 <kuma.tola3@gmail.com>
2017-08-27 18:02:37 +0900

    generatorの追加

--- G.Key/tests/generator.cc
+++ G.Key/tests/generator.cc
@@ -5,28 +5,50 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <cassert>
+#include <set>
 
 using namespace std;
 
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
 
-void output(int N, int M, const vector<int> &a, const string &prefix, int suffix){
+void output(int N, int M, const vector<string> &a, const string &prefix, int suffix){
+    assert(M + 1 == static_cast<int>(a.size()));
     char name[100];
     sprintf(name, "%s_%02d.in", prefix.c_str(), suffix);
     ofstream ofs(name);
     ofs << N << ' ' << M << endl;
-    for (int i = 0; i < N; ++i) {
-        ofs << a[i] << (i + 1 == N ? '\n' : ' ');
+    for(int i = 0; i < M + 1; i++){
+        ofs << a[i] << endl;
     }
     ofs.close();
 }
 
-vector<int> gen(int N) {
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        A[i] = rnd.next(MIN_A, MAX_A);
+string createT(int N, int M, vector<string> v){
+    string res;
+    rep(i,N){
+        res += v[static_cast<int>(rnd.next(0,M - 1))][i];
     }
-    return A;
+    return res;
+}
+
+vector<string> createString(int N, int M){
+    vector<string> res;
+    set<char> s[16]; //同じ文字が含まれるのを防ぐ
+    rep(i,M){
+        string tmp;
+        rep(j,N){
+            char add;
+            while(true){
+                add = static_cast<char>('a' + static_cast<int>(rnd.next(0,25)));
+                if(not s[j].count(add)) break;
+            }
+            s[j].insert(add);
+            tmp += add;
+        }
+        res.push_back(tmp);
+    }
+    res.push_back(createT(N, M, res));
+    return res;
 }
 
 int main(){
@@ -34,48 +56,27 @@ int main(){
     // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
     rnd.setSeed(time(0)+getpid());
 
-    // 乱数ケース
-    for(int i = 0; i < 10; ++i){
+    //小さいケース
+    for(int i = 0; i < 10; i++){
+        int N = rnd.next(MIN_N, 3);
+        int M = rnd.next(MIN_M, 3);
+        vector<string> v = createString(N, M);
+        output(N, M, v, "random_small", i);
+    }
+
+    //大きいケース
+    for(int i = 0; i < 10; i++){
+        int N = rnd.next(13, MAX_N);
+        int M = rnd.next(13, MAX_M);
+        vector<string> v = createString(N, M);
+        output(N, M, v, "random_large", i);
+    }
+
+    //ランダム
+    for(int i = 0; i < 10; i++){
         int N = rnd.next(MIN_N, MAX_N);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "50_random", i);
+        int M = rnd.next(MIN_M, MAX_M);
+        vector<string> v = createString(N, M);
+        output(N, M, v, "random", i);
     }
-
-    // 色の数Nが少ない乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, 10);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "51_random_Nsmall", i);
-    }
-
-    // 最大( LCM(N,M)が大きく、数の範囲の総和も大きい )ケース
-    vector<int> primenums;
-    const auto isPrime = [](int n){
-        if(n<=1)return false;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    };
-    for(int i=900;i<=1000;i++){
-        if(isPrime(i)){
-            primenums.push_back(i);
-        }
-    }
-
-    for(int i = 0; i < 10; ++i){
-        int pos = rnd.next(1,(int)primenums.size()-1);
-        int N = primenums[ pos ];
-        int M = primenums[ rnd.next(0,pos-1) ];
-        assert(M<=N);
-        vector<int> A(N);
-        for(int j=0;j<N;j++){
-            if(j%2==0)A[j]=0;
-            else A[j]=100;
-        }
-        output(N, M, A, "52_NMprime", i);
-    }
-
 }
