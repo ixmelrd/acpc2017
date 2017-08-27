3ecec8be16c90d9ce3a1c315ed94c7730b55fded
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-27 08:00:40 +0000

    Fix constraints for B.All_Japan

--- B.All_Japan/tests/constraints.hpp
+++ B.All_Japan/tests/constraints.hpp
@@ -1,5 +1,7 @@
 #define MIN_N 1
-#define MAX_N 1000
-#define MIN_M 1
-#define MIN_A 0
-#define MAX_A 100
+#define MAX_N 100000
+#define MIN_D 1
+#define MAX_D 100000
+#define MAX_T 1000000
+#define MIN_F 1
+#define MAX_F 1000000
