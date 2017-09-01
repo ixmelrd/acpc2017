117cff9b5e8c447372ff4b04c083ab00396223ea
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-01 04:08:36 +0000

    Add Ksmall case for C.Num_Tweet

--- C.Num_Tweet/tests/generator.cc
+++ C.Num_Tweet/tests/generator.cc
@@ -25,13 +25,23 @@ void case_50_Nsmall() {
   }
 }
 
-void case_51_Nlarge() {
+void case_51_Nsmall_Ksmall() {
+  rep(num, 10) {
+    int N = rnd.next(MIN_N, 20);
+    int K = rnd.next(MIN_K, 3);
+    vector<int> A(N);
+    rep(i, N) A[i] = rnd.next(MIN_A, i);
+    output(N, K, A, "51_Nsmall_Ksmall", num);
+  }
+}
+
+void case_52_Nlarge() {
   rep(num, 10) {
     int N = rnd.next(MAX_N / 2, MAX_N);
     int K = rnd.next(MIN_K, MAX_K);
     vector<int> A(N);
     rep(i, N) A[i] = rnd.next(MIN_A, i);
-    output(N, K, A, "51_Nlarge", num);
+    output(N, K, A, "52_Nlarge", num);
   }
 }
 
@@ -62,7 +72,8 @@ void case_61_corner() { // single node
 int main() {
   rnd.setSeed(time(0) + getpid());
   case_50_Nsmall();
-  case_51_Nlarge();
+  case_51_Nsmall_Ksmall();
+  case_52_Nlarge();
   case_59_random();
   case_60_corner();
   case_61_corner();
