ae997f65c376e190ca23691cd6bbb7b28f4c6b2a
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-01 04:06:07 +0000

    Update test case file name

--- C.Num_Tweet/tests/generator.cc
+++ C.Num_Tweet/tests/generator.cc
@@ -15,55 +15,55 @@ void output(const int N, const int K, const vector<int> &A, const string &prefix
   ofs.close();
 }
 
-void case_50_random() {
-  rep(num, 10) {
-    int N = rnd.next(MIN_N, MAX_N);
-    int K = rnd.next(MIN_K, MAX_K);
-    vector<int> A(N);
-    rep(i, N) A[i] = rnd.next(MIN_A, i);
-    output(N, K, A, "50_random", num);
-  }
-}
-
-void case_51_N_small() {
+void case_50_Nsmall() {
   rep(num, 10) {
     int N = rnd.next(MIN_N, 20);
     int K = rnd.next(MIN_K, MAX_K);
     vector<int> A(N);
     rep(i, N) A[i] = rnd.next(MIN_A, i);
-    output(N, K, A, "51_Nsmall", num);
+    output(N, K, A, "50_Nsmall", num);
   }
 }
 
-void case_60_corner() { // larger N
+void case_51_Nlarge() {
   rep(num, 10) {
     int N = rnd.next(MAX_N / 2, MAX_N);
     int K = rnd.next(MIN_K, MAX_K);
     vector<int> A(N);
     rep(i, N) A[i] = rnd.next(MIN_A, i);
-    output(N, K, A, "60_corner", num);
+    output(N, K, A, "51_Nlarge", num);
   }
 }
 
-void case_61_corner() { // stack overflow attack (anti dfs)
+void case_59_random() {
+  rep(num, 10) {
+    int N = rnd.next(MIN_N, MAX_N);
+    int K = rnd.next(MIN_K, MAX_K);
+    vector<int> A(N);
+    rep(i, N) A[i] = rnd.next(MIN_A, i);
+    output(N, K, A, "59_random", num);
+  }
+}
+
+void case_60_corner() { // stack overflow attack (anti dfs)
   int N = MAX_N, K = MAX_K;
   vector<int> A(N);
   rep(i, N) A[i] = i;
-  output(N, K, A, "61_corner");
+  output(N, K, A, "60_corner");
 }
 
-void case_62_corner() { // single node
+void case_61_corner() { // single node
   int N = 1, K = MIN_K;
   vector<int> A(N);
   A[0] = 0;
-  output(N, K, A, "62_corner");
+  output(N, K, A, "61_corner");
 }
 
 int main() {
   rnd.setSeed(time(0) + getpid());
-  case_50_random();
-  case_51_N_small();
+  case_50_Nsmall();
+  case_51_Nlarge();
+  case_59_random();
   case_60_corner();
   case_61_corner();
-  case_62_corner();
 }
