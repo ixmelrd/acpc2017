53582514f4892c87f8a3cc1023992f2808ed2de6
ixmel <kazuyakazuya524@gmail.com>
2017-09-05 18:26:27 +0900

    EのFF版追加

--- E.F_is_F_of_Flow/ixmel-TLE/main.cc
+++ E.F_is_F_of_Flow/ixmel-TLE/main.cc
@@ -47,23 +47,38 @@ class MF{//max flow
 	public:
 	int n;
 	vector<vector<edge> >G;//[MAX];
-	vector<bool>used;//[MAX];
+	vi level,iter;//[MAX];
 	MF(int size){
 		n=size;
 		G=vector<vector<edge> >(n);
 	}
 	void add_edge(int from, int to, int cap){
-		edge q={to,cap,int(G[to].size()),1};
+		edge q={to,cap,int(G[to].size())};
 		G[from].push_back(q);
-		q={from,0,int(G[from].size()-1),0};
+		q={from,0,int(G[from].size()-1)};
 		G[to].push_back(q);
+	}
+	void bfs(int s){
+		level=vi(n,-1);
+		queue<int>q;
+		level[s]=0;
+		q.push(s);
+		while(!q.empty()){
+			int v=q.front();q.pop();
+			for(int i=0;i<G[v].size();i++){
+				edge &e=G[v][i];
+				if(e.cap>0&&level[e.to]<0){
+					level[e.to]=level[v]+1;
+					q.push(e.to);
+				}
+			}
+		}
 	}
 	int dfs(int v,int t, int f) {
 		if(v==t)return f;
-		used[v]=1;
-		for(int i=0;i<G[v].size();i++){
+		for(int &i=iter[v];i<G[v].size();i++){
 			edge &e=G[v][i];
-			if(used[e.to]||e.cap<=0) continue;
+			if(level[v]>=level[e.to]||e.cap<=0) continue;
 			int d =dfs(e.to,t,min(f,e.cap));
 			if(d>0){
 				e.cap-=d;
@@ -74,12 +89,13 @@ class MF{//max flow
 		return 0;
 	}
 	int mf(int s,int t) {//from s to t,ford_fulkerson
-		int flow=0,f;
+		int flow=0;
 		while(1){
-			used=vector<bool>(n,false);
-			f=dfs(s,t,inf);
-			if(f==0)return flow;
-			flow+=f;
+			bfs(s);
+			if(level[t]<0)return flow;
+			iter=vi(n);
+			int f;
+			while((f=dfs(s,t,inf))>0)flow+=f;
 			if(flow>=10010)return -1;
 		}
 	}
