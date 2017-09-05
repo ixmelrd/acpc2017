14d3ff6ec2d720c05a92a386b2aa6ac401b3e7ae
ixmel <kazuyakazuya524@gmail.com>
2017-09-05 14:09:20 +0900

    Gのvalidatorを修正

--- G.Key/tests/validator.cc
+++ G.Key/tests/validator.cc
@@ -6,26 +6,16 @@
 #include <set>
 
 int main(){
-    registerValidation();
-    int n = inf.readInt(MIN_N, MAX_N);
-    inf.readSpace();
-    int m = inf.readInt(MIN_M, MAX_M);
-    inf.readEoln();
+	registerValidation();
+	int n = inf.readInt(MIN_N, MAX_N);
+	inf.readSpace();
+	int m = inf.readInt(MIN_M, MAX_M);
+	inf.readEoln();
 
-    std::vector<std::string> in;
-    for(int i = 0; i < m + 1; i++){
-        std::string s = inf.readToken("[a-z]+");
-        in.push_back(s);
-        assert(s.size() == n);
-        inf.readEoln();
-    }
     for(int i = 0; i < n; i++){
-		std::vector<int>h(256);
-        for(int j = 0; j < m; j++){
-			assert(h[in[j][i]]==0);
-			h[in[j][i]]=1;
-        }
-    }
-
-    inf.readEof();
+		if(i)inf.readSpace();
+		int t = inf.readInt(0,m-1);
+	}
+	inf.readEoln();
+	inf.readEof();
 }
