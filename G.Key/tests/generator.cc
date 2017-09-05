4108951c5adcf1a7d600f2d1450dc0b06dac92f4
野井 <kuma.tola3@gmail.com>
2017-09-05 13:56:57 +0900

    文字列ではなく数字を生成する

--- G.Key/tests/generator.cc
+++ G.Key/tests/generator.cc
@@ -11,7 +11,7 @@ using namespace std;
 
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
 
-void output(int N, int M, const vector<string> &a, const string &prefix, int suffix){
+void output(int N, int M, const vector<int> &a, const string &prefix, int suffix){
     assert(M + 1 == static_cast<int>(a.size()));
     char name[100];
     sprintf(name, "%s_%02d.in", prefix.c_str(), suffix);
@@ -23,31 +23,11 @@ void output(int N, int M, const vector<string> &a, const string &prefix, int suf
     ofs.close();
 }
 
-string createT(int N, int M, vector<string> v){
-    string res;
+vector<int> create(int N, int M){
+    vector<int> res(N);
     rep(i,N){
-        res += v[static_cast<int>(rnd.next(0,M - 1))][i];
+        res[i] = rnd.next(0, M);
     }
-    return res;
-}
-
-vector<string> createString(int N, int M){
-    vector<string> res;
-    set<char> s[16]; //同じ文字が含まれるのを防ぐ
-    rep(i,M){
-        string tmp;
-        rep(j,N){
-            char add;
-            while(true){
-                add = static_cast<char>('a' + static_cast<int>(rnd.next(0,25)));
-                if(not s[j].count(add)) break;
-            }
-            s[j].insert(add);
-            tmp += add;
-        }
-        res.push_back(tmp);
-    }
-    res.push_back(createT(N, M, res));
     return res;
 }
 
@@ -60,7 +40,7 @@ int main(){
     for(int i = 0; i < 10; i++){
         int N = rnd.next(MIN_N, 4);
         int M = rnd.next(MIN_M, 4);
-        vector<string> v = createString(N, M);
+        vector<int> v = create(N, M);
         output(N, M, v, "50_random_small", i);
     }
 
@@ -68,7 +48,7 @@ int main(){
     for(int i = 0; i < 10; i++){
         int N = rnd.next(12, MAX_N);
         int M = rnd.next(12, MAX_M);
-        vector<string> v = createString(N, M);
+        vector<int> v = create(N, M);
         output(N, M, v, "50_random_large", i);
     }
 
@@ -76,7 +56,7 @@ int main(){
     for(int i = 0; i < 10; i++){
         int N = rnd.next(MIN_N, MAX_N);
         int M = rnd.next(MIN_M, MAX_M);
-        vector<string> v = createString(N, M);
+        vector<int> v = create(N, M);
         output(N, M, v, "50_random", i);
     }
 
@@ -84,7 +64,7 @@ int main(){
     for(int i = 0; i < 3; i++){
         int N = MAX_N;
         int M = MAX_M;
-        vector<string> v = createString(N, M);
+        vector<int> v = create(N, M);
         output(N, M, v, "60_maximum", i);
     }
 
@@ -92,7 +72,7 @@ int main(){
     for(int i = 0; i < 3; i++){
         int N = MIN_N;
         int M = MIN_M;
-        vector<string> v = createString(N, M);
+        vector<int> v = create(N, M);
         output(N, M, v, "60_minimum", i);
     }
 }
