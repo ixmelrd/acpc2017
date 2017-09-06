ff49416728497a5db2228f655fefb4acc5e0f286
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-06 10:49:18 +0000

    Update vv solution for A.Shuttle_Run

--- A.Shuttle_Run/vv/main.cc
+++ A.Shuttle_Run/vv/main.cc
@@ -4,9 +4,8 @@ using namespace std;
 
 int main() {
   int n;
-  cin >> n;
   string s;
-  cin >> s;
+  cin >> n >> s;
   size_t pos = s.find("xx");
   cout << (pos == string::npos ? n : static_cast<int>(pos) + 1) << endl;
 }
