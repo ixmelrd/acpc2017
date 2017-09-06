be7d5fd250fa3ef0aacdb1cd191cc736a8028a73
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-06 22:03:33 +0000

    Fix validator for D.Noroi_of_Jigen

--- D.Noroi_of_Jigen/tests/validator.cc
+++ D.Noroi_of_Jigen/tests/validator.cc
@@ -1,6 +1,54 @@
 #include "./constraints.hpp"
 #include "./testlib.h"
 #include <cassert>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#define loop(i, a, n) for (int i = (a); i < (n); i++)
+#define rep(i, n) loop(i, 0, n)
+template<typename A, typename B> bool cmin(A &a, const B &b) { return a > b ? (a = b, true) : false; }
+using namespace std;
+
+using Weight = int;
+struct Edge {
+  int src, dst;
+  Weight weight;
+  Edge(const int s, const int d, const Weight w) : src(s), dst(d), weight(w) {}
+};
+
+using Edges = std::vector<Edge>;
+class Graph {
+  std::vector<Edges> g;
+
+public:
+  Graph(const int size = 0) : g(size) {}
+  size_t size() const { return g.size(); }
+  const Edges &operator[](const int i) const { return g[i]; }
+  void addArc(const int src, const int dst, const Weight w) { g[src].emplace_back(src, dst, w); }
+  void addEdge(const int node1, const int node2, const Weight w) {
+    addArc(node1, node2, w);
+    addArc(node2, node1, w);
+  }
+};
+
+template<int inf> std::vector<Weight> dijkstra(const Graph &g, const int src) {
+  using state = std::pair<Weight, int>;
+  std::priority_queue<state, std::vector<state>, std::greater<state>> q;
+  std::vector<Weight> dist(g.size(), inf);
+  dist[src] = 0;
+  q.emplace(0, src);
+  while (q.size()) {
+    Weight d;
+    int v;
+    std::tie(d, v) = q.top();
+    q.pop();
+    if (dist[v] < d) continue;
+    for (auto &e : g[v]) {
+      if (cmin(dist[e.dst], dist[v] + e.weight)) q.emplace(dist[e.dst], e.dst);
+    }
+  }
+  return dist;
+}
 
 int main() {
   registerValidation();
@@ -10,20 +58,32 @@ int main() {
   inf.readSpace();
   int s = inf.readInt(1, N);
   inf.readSpace();
-  int d = inf.readInt(1, N);
-  assert(s != d);
+  int t = inf.readInt(1, N);
   inf.readEoln();
-  for (int i = 0; i < N; i++) {
-    if (i) inf.readSpace();
-    inf.readInt(MIN_d, MAX_d);
+  vector<int> d(N);
+  rep(i, N) {
+    d[i] = inf.readInt(MIN_d, MAX_d);
+    if (i < N - 1) {
+      inf.readSpace();
+    } else {
+      inf.readEoln();
+    }
   }
-  inf.readEoln();
-  for (int i = 0; i < M; i++) {
-    int a = inf.readInt(1, N);
+  vector<int> a(M), b(M);
+  rep(i, M) {
+    a[i] = inf.readInt(1, N);
     inf.readSpace();
-    int b = inf.readInt(1, N);
+    b[i] = inf.readInt(1, N);
     inf.readEoln();
-    assert(a < b);
   }
   inf.readEof();
+
+  assert(s != t);
+  rep(i, M) assert(a[i] < b[i]);
+  Graph g(N);
+  loop(i, 1, N) g.addArc(i, i - 1, 0);
+  rep(i, M) g.addArc(a[i] - 1, b[i] - 1, d[b[i] - 1]);
+  constexpr int INF = MAX_d * MAX_N;
+  bool isReachable = dijkstra<INF>(g, s - 1)[t - 1] != INF;
+  assert(isReachable);
 }
