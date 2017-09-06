907a0383caacbe3128ed3ce373f825f7255cef20
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-06 14:11:45 +0000

    Improve generator for C.Num_Tweet

--- C.Num_Tweet/tests/generator.cc
+++ C.Num_Tweet/tests/generator.cc
@@ -67,6 +67,16 @@ void case_54_Nlargest() {
   }
 }
 
+void case_55_Nsmallest() {
+  int N = MIN_N;
+  rep(num, 10) {
+    int K = rnd.next(MIN_K, MAX_K);
+    vector<int> A(N);
+    A[0] = 0;
+    output(N, K, A, "55_Nsmallest", num);
+  }
+}
+
 void case_59_random() {
   rep(num, 10) {
     int N = rnd.next(MIN_N, MAX_N);
@@ -87,12 +97,12 @@ void case_60_corner() { // stack overflow attack (anti dfs)
   }
 }
 
-void case_61_corner() { // single node
-  int N = 1;
+void case_61_corner() { // single node trees
   rep(num, 10) {
-    int K = rnd.next(MIN_K, MAX_K);
+    int N = num == 0 ? MAX_N : rnd.next(MAX_N - 100, MAX_N);
+    int K = num == 0 ? MAX_K : rnd.next(MAX_K - 100, MAX_K);
     vector<int> A(N);
-    A[0] = 0;
+    rep(i, N) A[i] = 0;
     output(N, K, A, "61_corner", num);
   }
 }
@@ -110,8 +120,8 @@ void case_62_corner() { // many large walks
 
 void case_63_corner() { // replied by all others
   rep(num, 10) {
-    int N = rnd.next(MAX_N - num, MAX_N);
-    int K = rnd.next(MIN_K, MIN_K + num);
+    int N = num == 0 ? MAX_N : rnd.next(MAX_N - 100, MAX_N);
+    int K = num == 0 ? MIN_K : rnd.next(MIN_K, MIN_K + 10);
     vector<int> A(N);
     A[0] = 0;
     loop(i, 1, N) A[i] = 1;
@@ -126,6 +136,7 @@ int main() {
   case_52_Nlarge();
   case_53_Nlargest_Ksmall();
   case_54_Nlargest();
+  case_55_Nsmallest();
   case_59_random();
   case_60_corner();
   case_61_corner();
