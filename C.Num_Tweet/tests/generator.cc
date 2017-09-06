00e04c4aa7cde6034381d30143471ffac3bb83c2
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-06 08:48:38 +0000

    Add test cases for C.Num_Tweet

--- C.Num_Tweet/tests/generator.cc
+++ C.Num_Tweet/tests/generator.cc
@@ -47,13 +47,23 @@ void case_52_Nlarge() {
   }
 }
 
-void case_53_Nlargest() {
+void case_53_Nlargest_Ksmall() {
+  int N = MAX_N;
   rep(num, 10) {
-    int N = MAX_N;
+    int K = rnd.next(MIN_K, 20);
+    vector<int> A(N);
+    rep(i, N) A[i] = rnd.next(MIN_A, i);
+    output(N, K, A, "53_Nlargest_Ksmall", num);
+  }
+}
+
+void case_54_Nlargest() {
+  int N = MAX_N;
+  rep(num, 10) {
     int K = rnd.next(MIN_K, MAX_K);
     vector<int> A(N);
     rep(i, N) A[i] = rnd.next(MIN_A, i);
-    output(N, K, A, "53_Nlargest", num);
+    output(N, K, A, "54_Nlargest", num);
   }
 }
 
@@ -103,7 +113,8 @@ int main() {
   case_50_Nsmall_Ksmall();
   case_51_Nsmall();
   case_52_Nlarge();
-  case_53_Nlargest();
+  case_53_Nlargest_Ksmall();
+  case_54_Nlargest();
   case_59_random();
   case_60_corner();
   case_61_corner();
