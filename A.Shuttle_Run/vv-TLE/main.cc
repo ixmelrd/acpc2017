e198a6f70cfaffb6bb85f54990ffb298cb6d8a4f
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-07 23:44:07 +0000

    Add TLE solution for A.Shuttle_Run

---
+++ A.Shuttle_Run/vv-TLE/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n == 1) {
+    cout << 1 << endl;
+    return 0;
+  }
+  string s;
+  cin >> s;
+  for (int i = 0; i < n - 1; i++) { // O(N)
+    if (s[0] == 'x' && s[1] == 'x') {
+      cout << i + 1 << endl;
+      return 0;
+    }
+    for (int j = 1; j < s.size(); j++) s[j - 1] = s[j]; // O(N)
+    s.pop_back();
+  }
+  cout << n << endl;
+}
