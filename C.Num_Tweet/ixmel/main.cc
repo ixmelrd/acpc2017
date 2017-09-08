cb0cd2dc2f8afc56a43929c416cbca381c092968
ixmel <kazuyakazuya524@gmail.com>
2017-09-08 15:41:15 +0900

    Cのジャッジ解追加

--- C.Num_Tweet/ixmel/main.cc
+++ C.Num_Tweet/ixmel/main.cc
@@ -1,30 +1,104 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<algorithm>	
+#include<map>
+#include<set>
+#include<utility>
+#include<cmath>
+#include<cstring>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<sstream>
+#include<iomanip>
+#include<assert.h>
 #define loop(i,a,b) for(int i=a;i<b;i++) 
 #define rep(i,a) loop(i,0,a)
+#define pb push_back
+#define all(in) in.begin(),in.end()
+#define shosu(x) fixed<<setprecision(x)
 using namespace std;
-const int inf = 1e9;
-typedef vector<int> vi;
-int main(){
-	int n,m;
-	cin>>n>>m;
-	int sum=0;
-	int now=0;
-	vi in(n);
-	rep(i,n)cin>>in[i];
-	do{
-		int mi=inf,ma=-inf;
-		rep(i,m){
-			int t=in[now];
-			mi=min(mi,t);
-			ma=max(ma,t);
-			
-			now++;
-			if(now==n)now=0;
+//kaewasuretyuui
+typedef long long ll;
+typedef int Def;
+typedef pair<Def,Def> pii;
+typedef vector<Def> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vp;
+typedef vector<vp> vvp;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef pair<Def,pii> pip;
+typedef vector<pip>vip;
+//#define mt make_tuple
+//typedef tuple<double,int,double> tp;
+//typedef vector<tp> vt;
+template<typename A,typename B>bool cmin(A &a,const B &b){return a>b?(a=b,true):false;}
+template<typename A,typename B>bool cmax(A &a,const B &b){return a<b?(a=b,true):false;}
+const double PI=acos(-1);
+const double EPS=1e-7;
+Def inf=sizeof(Def)==sizeof(long long)?2e18:1e9;
+int dx[]={0,1,0,-1};
+int dy[]={1,0,-1,0};
+class DIJ{
+	public:
+	struct edge{
+		int to,cost;
+	};
+	vector<vector<edge> >G;
+	int n;
+	vi d;//distance
+	DIJ(int size){
+		n=size;
+		G=vector<vector<edge> >(n);
+	}
+	void add_edge(int a,int b,int c){
+		edge e={b,c},ee={a,c};
+		G[a].pb(e);
+//		G[b].pb(ee);
+	}
+	void dij(int s){
+		d=vi(n,inf);
+		d[s]=0;
+		priority_queue<pii>q;
+		q.push(pii(0,s));
+		while(!q.empty()){
+			pii p=q.top();
+			q.pop();
+			int pos=p.second,cost=-p.first;
+			if(cost>d[pos])continue;
+			rep(i,G[pos].size()){
+				edge e=G[pos][i];
+				int to=e.to;
+				int nowcost=cost+e.cost;
+				if(nowcost<d[to]){
+					d[to]=nowcost;
+					q.push(pii(-d[to],to));
+				}
+			}
 		}
-		sum+=ma-mi;
-	}while(now);
-	cout<<sum<<endl;
+	}
+};
+int main(){
+	int n,k;
+	cin>>n>>k;
+	DIJ dij(n+1);
+	vi cnt(n);
+	rep(i,n){
+		int a;
+		cin>>a;
+		if(a){
+			dij.add_edge(i,a-1,1);
+			cnt[a-1]++;
+		}else dij.add_edge(n,i,1);
+	}
+	rep(i,n)if(!cnt[i])dij.add_edge(n,i,1);
+	dij.dij(n);
+	int out=0;
+	rep(i,n)if(dij.d[i]<=k)out++;
+	cout<<out<<endl;
 }
 
 
