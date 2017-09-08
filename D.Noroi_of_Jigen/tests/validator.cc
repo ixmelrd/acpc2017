72b21c8c0c174962ca846d4d0c57fa7f51e54736
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-09-08 11:26:11 +0000

    Update validator for D.Noroi_of_Jigen

--- D.Noroi_of_Jigen/tests/validator.cc
+++ D.Noroi_of_Jigen/tests/validator.cc
@@ -8,6 +8,7 @@
 #include <tuple>
 #define loop(i, a, n) for (int i = (a); i < (n); i++)
 #define rep(i, n) loop(i, 0, n)
+#define all(v) (v).begin(), (v).end()
 using pii = std::pair<int, int>;
 template<typename A, typename B> bool cmin(A &a, const B &b) { return a > b ? (a = b, true) : false; }
 using namespace std;
@@ -53,14 +54,148 @@ template<int inf> std::vector<Weight> dijkstra(const Graph &g, const int src) {
   return dist;
 }
 
-bool isReachable(int N, int M, int s, int t, vector<int> a, vector<int> b) {
+template<typename Monoid> constexpr typename Monoid::value_type powm(typename Monoid::value_type a, int b) {
+  Monoid m;
+  typename Monoid::value_type c = m.id();
+  while (b) {
+    if (b & 1) c = m(c, a);
+    b >>= 1;
+    a = m(a, a);
+  }
+  return c;
+}
+
+template<typename LSTTrait> class LazySegTree {
+  using FoldMonoid = typename LSTTrait::FoldMonoid;
+  using ActionMonoid = typename LSTTrait::ActionMonoid;
+  using T = typename FoldMonoid::value_type;
+  using Q = typename ActionMonoid::value_type;
+
+  LSTTrait lstTrait; // apply :: (T, Q) -> T
+  FoldMonoid foldM;
+  ActionMonoid actionM;
+  int size = 1;
+  std::vector<T> tree; // 1-indexed
+  std::vector<Q> lazy; // 1-indexed
+
+  void flush(int i, int l, int r) {
+    tree[i] = lstTrait.apply(tree[i], powm<ActionMonoid>(lazy[i], r - l));
+    if (i < size) {
+      lazy[i * 2] = actionM(lazy[i * 2], lazy[i]);
+      lazy[i * 2 + 1] = actionM(lazy[i * 2 + 1], lazy[i]);
+    }
+    lazy[i] = actionM.id();
+  }
+
+public:
+  LazySegTree(const int n = 0) {
+    while (size < n) size *= 2;
+    tree.assign(size * 2, foldM.id());
+    lazy.assign(size * 2, actionM.id());
+  }
+
+  template<typename InputIterator> LazySegTree(InputIterator first, InputIterator last) {
+    int n = std::distance(first, last);
+    while (size < n) size *= 2;
+    tree.resize(size * 2, foldM.id());
+    lazy.resize(size * 2, actionM.id());
+    std::copy(first, last, tree.begin() + size);
+    for (int i = size - 1; i >= 1; i--) tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
+  }
+
+  // [l, r)
+  void act(int l, int r, const Q x) { act(l, r, x, 1, 0, size); }
+  void act(const int l, const int r, const Q x, int i, int a, int b) {
+    flush(i, a, b);
+    if (b <= l || r <= a) return;
+    if (l <= a && b <= r) {
+      lazy[i] = actionM(lazy[i], x);
+      flush(i, a, b);
+      return;
+    }
+    act(l, r, x, i * 2, a, (a + b) / 2);
+    act(l, r, x, i * 2 + 1, (a + b) / 2, b);
+    tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
+  }
+
+  // [l, r)
+  T fold(int l, int r) { return fold(l, r, 1, 0, size); }
+  T fold(int l, int r, int i, int a, int b) {
+    flush(i, a, b);
+    if (b <= l || r <= a) return foldM.id();
+    if (l <= a && b <= r) return tree[i];
+    T lhs = fold(l, r, i * 2, a, (a + b) / 2);
+    T rhs = fold(l, r, i * 2 + 1, (a + b) / 2, b);
+    tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
+    return foldM(lhs, rhs);
+  }
+};
+
+template<typename T, T upperInf = std::numeric_limits<T>::max()> struct minMonoid {
+  using value_type = T;
+  constexpr value_type id() { return upperInf; }
+  constexpr value_type operator()(const value_type &a, const value_type &b) { return a < b ? a : b; }
+};
+
+template<typename T> struct updateMonoid {
+  using value_type = pair<bool, T>;
+  constexpr value_type id() { return {false, T()}; }
+  constexpr value_type operator()(const value_type &a, const value_type &b) { return b.first ? b : a; }
+};
+
+template<typename T, T upperInf = std::numeric_limits<T>::max()> struct RMQ_RUQ_Trait {
+  using FoldMonoid = minMonoid<T, upperInf>;
+  using ActionMonoid = updateMonoid<T>;
+  typename FoldMonoid::value_type apply(typename FoldMonoid::value_type t, typename ActionMonoid::value_type q) { return q.first ? q.second : t; }
+};
+
+class RangeUnionFind {
+  LazySegTree<RMQ_RUQ_Trait<int>> lst;
+
+public:
+  RangeUnionFind(const int n) {
+    vector<int> ufInit(n);
+    iota(all(ufInit), 0);
+    lst = LazySegTree<RMQ_RUQ_Trait<int>>(all(ufInit));
+  }
+
+  int root(int x) {
+    int a = lst.fold(x, x + 1);
+    if (a == x) return x;
+    int b = root(a);
+    lst.act(x, x + 1, {true, b});
+    return b;
+  }
+
+  bool same(const int a, const int b) { return root(a) == root(b); }
+
+  void unite(int a, int b) { // unite [a, b)
+    int r = root(a);
+    if (a == b) return;
+    lst.act(a, b, {true, r});
+  }
+};
+
+bool isReachable(const int N, const int M, const int s, const int t, const vector<int> &a, const vector<int> &b) {
   if (t < s) return true;
-  Graph g(N);
-  loop(i, 1, N) g.addArc(i, i - 1, 0);
-  rep(i, M) g.addArc(a[i] - 1, b[i] - 1, 0);
-  constexpr int INF = 1;
-  bool isReachable = dijkstra<INF>(g, s - 1)[t - 1] != INF;
-  return isReachable;
+  bool isReachableByDijkstra, isReachableByRangeUnionFind;
+  {
+    Graph g(N);
+    loop(i, 1, N) g.addArc(i, i - 1, 0);
+    rep(i, M) g.addArc(a[i] - 1, b[i] - 1, 0);
+    constexpr int INF = 1;
+    isReachableByDijkstra = dijkstra<INF>(g, s - 1)[t - 1] != INF;
+  }
+  {
+    RangeUnionFind uf(N);
+    rep(i, M) uf.unite(a[i] - 1, b[i]);
+    isReachableByRangeUnionFind = uf.same(s - 1, t - 1);
+  }
+  if (isReachableByDijkstra != isReachableByRangeUnionFind) {
+    cout << "The validator is something wrong. Please report it to vvataarne." << endl;
+    return 1;
+  }
+  return isReachableByDijkstra;
 }
 
 int main() {
