ae990481f0345119178440e84bcfda510fae3595
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-09-11 01:09:43 +0900

    GのT.Mの回答を2つ修正

--- G.Key/T.M/main.c
+++ G.Key/T.M/main.c
@@ -11,7 +11,7 @@ void in(int *d,int a){
 }
 void f(int *a,int *b,int n){//printf("f\n");
   int i;
-  for(i=0;i<4;i++){
+  for(i=0;i<5;i++){
     if(a[i]==n){
       a[i]=0;
       a[5]-=n;
@@ -19,7 +19,17 @@ void f(int *a,int *b,int n){//printf("f\n");
       return ;
     }
   }
-}    
+}
+int p(int a,int b,int c,int d){//printf("%d %d %d %d\n",a,b,c,d);
+  int s=0;
+  b-=a;
+  d-=c;
+  s+=(a>b?b:(a+b)/2);
+  a=MAX(0,a-b);
+  s+=(c>d?d:(c+d)/2);
+  c=MAX(0,c-d);
+  return s+abs(a-c)/2+a+c;
+}
 int main(){
   int n,i,j[3],k,l,o,max[2],ans=1e9,sum[2],a[2][6],b[2][6],c=0,t,d[2][6]={};
   scanf("%d %d",&n,&m);
@@ -33,6 +43,7 @@ int main(){
     for(k=0;k<6;k++)printf("%d ",d[i][k]);
     printf("\n");
     }printf("%d\n",c);//*/
+  //printf("%d\n",c);
   if(c>3){
     printf("%d\n",(d[0][5]+d[1][5]+c*m/2)/2);
     return 0;
@@ -44,12 +55,23 @@ int main(){
     for(i=0;i<l;i++)in(a[0],m/2);
     for(;   i<c;i++)in(a[1],m/2);
 
-    for(i=t=0;i<2;i++){
-      t+=a[i][0]>a[i][5]?a[i][5]:(a[i][5]+a[i][0])/2;
-      max[i]=MAX(0,a[i][0]-a[i][5]);
+    if(a[0][5]%2==a[1][5]%2){
+      if(a[0][5]%2){
+	ans=MIN(ans,p(a[0][0]-1,a[0][5]-1,a[1][0]  ,a[1][5]+1)+1);
+	ans=MIN(ans,p(a[0][0]  ,a[0][5]+1,a[1][0]-1,a[1][5]-1)+1);
+      }
+      else ans=MIN(ans,p(a[0][0],a[0][5],a[1][0],a[1][5]));
+    }      
+
+      
+    /*  for(i=t=0;i<2;i++){
+      //for(k=0;k<6;k++)printf("%d ",a[i][k]);printf("\n");
+      t+=a[i][0]*2>a[i][5]?a[i][5]-a[i][0]:a[i][5]/2;
+      max[i]=MAX(0,a[i][0]*2-a[i][5]);
     }
-    ans=MIN(ans,t+abs(max[0]-max[1])/2+max[0]+max[1]);//*/
-   
+    if(a[0][5]%2==a[1][5]%2)ans=MIN(ans,t+abs(max[0]-max[1])/2+max[0]+max[1]);
+//*/
+    //printf("%d\n",ans);
     /*for(i=0;i<2;i++){
       for(k=0;k<6;k++)printf("%d ",d[i][k]);
       printf("\n");
@@ -66,26 +88,37 @@ int main(){
 	    for(o=0;o<6;o++)b[k][o]=a[k][o];
 	  }
 	  
-	  for(k=0;k<3;k++){
+	  for(k=0;k<3;k++){//printf(":%d ",j[k]);
 	    if(j[k]>=n)continue;//printf("%d:\n",s[j[k]]);
-	    if(s[j[k]]<m/2)f(b[0],b[1],s[j[k]]);
-	    else           f(b[1],b[0],m-s[j[k]]);
-	  }
+	    //printf("::%d\n",s[j[k]]);
+	    if(s[j[k]]<(m+1)/2)f(b[0],b[1],s[j[k]]);
+	    else               f(b[1],b[0],m-s[j[k]]);
+	    //for(o=0;o<6;o++)printf("%d ",b[0][o]);printf("!\n");
+	    //for(o=0;o<6;o++)printf("%d ",b[1][o]);printf("!\n");
+	  }//printf("\n");
 	  for(o=t=0;o<2;o++){
 	    //for(k=0;k<6;k++)printf("%d ",b[o][k]);printf("\n");
 	    
 	    for(i=max[o]=0;i<4;i++)max[o]=MAX(max[o],b[o][i]);
+	  }
+	  /*
 	    sum[o]=b[o][5]-max[o];
 	    t+=max[o]>sum[o]?sum[o]:(sum[o]+max[o])/2;
 	    max[o]=MAX(0,max[o]-sum[o]);
 	    //printf("%d %d ",max[o],sum[o]);
-	  }//printf("\n");
-	  if((max[0]+sum[0])%2-(max[1]+sum[1])%2)continue;
-	  ans=MIN(ans,t+abs(max[0]-max[1])/2+max[0]+max[1]);//*/
+	    }//printf("\n");//*/
+	  //printf("!!%d %d\n",max[0],max[1]);
+	  if(b[0][5]%2-b[1][5]%2)continue;
+	  if(b[0][5]%2){
+	    ans=MIN(ans,p(max[0]-1,b[0][5]-1,max[1]  ,b[1][5]+1)+1);
+	    ans=MIN(ans,p(max[0]  ,b[0][5]+1,max[1]-1,b[1][5]-1)+1);
+	  }
+	  else ans=MIN(ans,p(max[0],b[0][5],max[1],b[1][5]));
+	  //printf("%d\n",ans);
 	}
       }
     }//*/
   }
-  printf("%d\n",ans);//*/
+  printf("%d\n",ans-1e9?ans:-1);//*/
   return 0;
 }
---
+++ G.Key/T.M_N4/main.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+int n,m,sum[2],max[2],c=0,d[100010]={};
+int MAX(int a,int b){return a<b?b:a;}
+int MIN(int a,int b){return a<b?a:b;}
+int p(int a,int b,int c,int d){//max sum max sum
+  int s=0;
+  b-=a;
+  d-=c;
+  s+=a>b?b:(a+b)/2;
+  a=MAX(0,a-b);
+  s+=c>d?d:(c+d)/2;
+  c=MAX(0,c-d);
+  return s+abs(a-c)/2+a+c;
+}
+int f(int i,int n){
+  sum[i]+=n;
+  max[i]=MAX(max[i],n);
+}
+int g(int i,int j,int k,int l){
+  int t,o,ans=1e9;
+  sum[0]=sum[1]=max[0]=max[1]=0;
+  for(o=0;o<n;o++){
+    if(m%2==0&&d[o]==m/2)continue;
+    if((d[o]<(m+1)/2)==(o==i||o==j||o==k))f(1,m-d[o]);
+    else                                  f(0,  d[o]);
+  }
+  for(o=0;o<l;o++)f(0,m/2);
+  for(   ;o<c;o++)f(1,m/2);
+  //printf("%d %d %d %d %d %d %d\n",d[i],d[j],d[k],max[0],sum[0],max[1],sum[1]);
+  if(sum[0]%2-sum[1]%2)return 1e9;
+  if(sum[0]%2){
+    ans=MIN(ans,p(max[0]-1,sum[0]-1,max[1]  ,sum[1]+1)+1);
+    ans=MIN(ans,p(max[0]  ,sum[0]+1,max[1]-1,sum[1]-1)+1);
+  }
+  else ans=p(max[0],sum[0],max[1],sum[1]);
+  return ans;
+}
+int main(){
+  int i,j,k,l,ans=1e9;
+  scanf("%d %d",&n,&m);
+  for(i=0;i<n;i++){
+    scanf("%d",&d[i]);
+    if(m%2==0&&d[i]==m/2)c++;
+  }
+  for(l=0;l<=c;l++){
+    ans=MIN(ans,g(n,n,n,l));//printf("%d\n",ans);
+    for(i=0;i<n;i++){
+      ans=MIN(ans,g(i,n,n,l));//printf("%d\n",ans);
+      for(j=i+1;j<n;j++){
+	ans=MIN(ans,g(i,j,n,l));//printf("%d\n",ans);
+	for(k=j+1;k<n;k++){ans=MIN(ans,g(i,j,k,l));}//printf("%d\n",ans);}
+      }
+    }
+  }
+  printf("%d\n",ans-1e9?ans:-1);
+  return 0;
+}
+	  
