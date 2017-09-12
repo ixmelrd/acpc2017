e631ae93f80b05377fef9c9a44e9dab452d5dddf
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-09-12 12:44:08 +0900

    FのT.Mの回答を修正

--- F.Step_hard/T.M2/main.c
+++ F.Step_hard/T.M2/main.c
@@ -5,7 +5,8 @@ int mbk(long long a,int n,int m){return n?(mbk((a*a)%m,n/2,m)*(n%2?a:1))%m:1;}
 int inv(int a,int m){return mbk((long long)a,m-2,m);}
 int aCb(int a,int b,int m){return ((d[a]*e[b])%m*e[a-b])%m;}
 int g(int n,int m,int k){
-  int a=(m-k)/2,s=aCb(m,a,M),i;//printf("%d %d\n",s,a);
+  int a=(m-k)/2,i;//printf("%d %d\n",s,a);
+  long long s=aCb(m,a,M);
   for(i=0;1;i++){
     a-=i%2?n:1;//printf("a %d\n",a);
     if(a<0)break;
@@ -17,11 +18,12 @@ int g(int n,int m,int k){
     if(a>m)return s;
     s=(s+aCb(m,a,M)*(i%2?1:-1)+M)%M;
   }
+  return  (int)s;
 }
 int f(int n,int m){
   int s=0,i;
   for(i=0;i<n;i++){
-    if(m%2-n%2)continue;
+    if(m%2-i%2)continue;
     s=(s+g(n,m,i))%M;
   }
   return s;
