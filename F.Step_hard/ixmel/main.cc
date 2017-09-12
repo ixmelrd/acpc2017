1938aa0d7fb869a204b88fd0b0a1b6071ec1bcf9
ixmel <kazuyakazuya524@gmail.com>
2017-09-12 11:50:22 +0900

    Fのジャッジ解追加

--- F.Step_hard/ixmel/main.cc
+++ F.Step_hard/ixmel/main.cc
@@ -32,33 +32,39 @@ ll nCr(ll n,ll r){
 	ll out=fact[n]*powmod(fact[r]*fact[n-r]%MOD,MOD-2)%MOD;
 	return out;
 }
-ll solve2(ll n,ll m,ll a,ll b){
-	ll out;
-	if(a<=n&&b<=m)out=(nCr(n+m,n)-nCr(n+m,m-b)-solve2(n-a,m+a,b,2*a+b)+MOD)%MOD;
-	else if(a<=n)out=(nCr(n+m,n)-nCr(n+m,n-a)+MOD)%MOD;
-	else if(b<=m)out=(nCr(n+m,n)-nCr(n+m,m-b)+MOD)%MOD;		
-	else out=nCr(n+m,n);
-
-	cout<<n<<" "<<m<<" "<<a<<" "<<b<<" "<<out<<endl;
-	return out;
-}
 ll solve(ll n,ll m){
 	if(n==1)return 0;
-	
-	ll out=0;
+	vi cnt(m+1);
 	rep(i,n)if((m+i)%2==0&&m-i>=0){
-		ll up=(m+i)/2,down=(m-i)/2;
 		//up+down=m
 		//up-down=i
 		// -> up=(m+i)/2 down=(m-i)/2
-		(out+=solve2(up,down,n,1))%=MOD;
+		ll up=(m+i)/2,down=(m-i)/2;
+		ll N[2]={n,1};
+		int t=up,q=1;
+		cnt[t]-=1;
+		while(t>=0){
+			if(q)cnt[t]+=1;
+			else cnt[t]-=1;
+			q^=1;
+			t-=N[q];
+		}
+		t=up,q=1;
+		while(t<=m){
+			if(q)cnt[t]+=1;
+			else cnt[t]-=1;
+			q^=1;
+			t+=N[q^1];
+		}
 	}
+	ll out=0;
+	rep(i,m+1)(out+=cnt[i]*nCr(m,i))%=MOD;
 	return out;
 }
 int main(){
 	init();
 	ll n,m;
 	cin>>n>>m;
-	cout<<solve(n,m)<<endl;
-//	cout<<(solve(n,m)-solve(n-1,m)+MOD)%MOD<<endl;
+//	cout<<solve(n,m)<<endl;
+	cout<<(solve(n,m)-solve(n-1,m)+MOD)%MOD<<endl;
 }
