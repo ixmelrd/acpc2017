b0a922cfc96c04834d3c629f986249af30f81e4f
yebityon <yebi@yebityonnoMacBook-Air.local>
2017-09-13 18:32:29 +0900

    generetorを修正

--- A.Shuttle_Run/tests/generator.cc
+++ A.Shuttle_Run/tests/generator.cc
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <cassert>
 #define TEST_CASE 10
-#define rangemax 1000000000
+#define rangemax 100000
 #define rangemin 1
 using namespace std;
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
@@ -15,40 +15,43 @@ void output(string s,int length,const string &name){
     ofstream ofs(name);
     ofs << length << endl;
     rep(i,s.size()){
-	    ofs << s[i];
+        ofs << s[i];
     }
     ofs << endl;
     ofs.close();
 }
 int main(){
-	rnd.setSeed(time(0)+getpid());
-	/*小さいテストケースを作成*/
-	rep(case_num,TEST_CASE){
-		char file_name[100];
-		sprintf(file_name,"50-small-%02d.in",case_num);
-		int length=rnd.next(MIN_N,20);
-		string s;
-		rep(i,length){
-			int num=rnd.next(1,100);
-			if(num%2||s.back()=='x')s.push_back('o');
-			else s.push_back('x');
-		}
-		output(s,length,file_name);
-	}
-    /*最大テストケースを
-     a作成*/
-    rep(case_num,TEST_CASE){
+    rnd.setSeed(time(0)+getpid());
+    /*小さいテストケースを作成*/
+    rep(case_num,5){
         char file_name[100];
-        sprintf(file_name,"50-big-%02d.in",case_num);
-        int length=rnd.next(1000,MAX_N);
+        sprintf(file_name,"50-small-%02d.in",case_num);
+        int length=rnd.next(MIN_N,20);
         string s;
         rep(i,length){
-            int num=rnd.next(rangemin,rangemax);
-            if(num%10||s.back()=='x')s.push_back('o');
+            int num=rnd.next(1,100);
+            if(num%10)s.push_back('o');
             else s.push_back('x');
         }
         output(s,length,file_name);
     }
+    rnd.setSeed(time(0)+getpid());
+    /*大きめ*/
+    rep(case_num,5){
+        
+        rnd.setSeed(time(0)+getpid());
+        char file_name[100];
+        sprintf(file_name,"50-large-%02d.in",case_num);
+        int length=rnd.next(MAX_N-1000,MAX_N);
+        string s;
+        rep(i,length){
+            int num=rnd.next(rangemin,rangemax);
+            if(num%10)s.push_back('o');
+            else s.push_back('x');
+        }
+        output(s,length,file_name);
+    }
+    rnd.setSeed(time(0)+getpid());
     /*最大テストケースを作成*/
     rep(case_num,TEST_CASE){
         char file_name[100];
@@ -57,12 +60,13 @@ int main(){
         string s;
         rep(i,length){
             int num=rnd.next(rangemin,rangemax);
-            if(num%10||s.back()=='x')s.push_back('o');
+            if(num%10)s.push_back('o');
             else s.push_back('x');
         }
         output(s,length,file_name);
     }
-	/*xの出現確率を50％*/
+    /*xの出現確率を50％*/
+    rnd.setSeed(time(0)+getpid());
     rep(case_num,TEST_CASE){
         char file_name[100];
         sprintf(file_name,"60-even-%02d.in",case_num);
@@ -70,22 +74,49 @@ int main(){
         string s;
         rep(i,length){
             int num=rnd.next(rangemin,rangemax);
-            if(num%2||s.back()=='x')s.push_back('o');
+            if(num%2)s.push_back('o');
             else s.push_back('x');
         }
         output(s,length,file_name);
     }
-	/*ランダムテストケース*/
-	rep(case_num,TEST_CASE){
-		char file_name[100];
-		sprintf(file_name,"60-random-%02d.in",case_num);
-		int length=rnd.next(MIN_N,MAX_N);
-		string s;
-		rep(i,length){
-			int num=rnd.next(rangemin,rangemax);
-			if(num%10||s.back()=='x')s.push_back('o');
-			else s.push_back('x');
-		}
-		output(s,length,file_name);
-	}
+    /*xが連続して2つでない*/
+    rep(case_num,5){
+        char file_name[100];
+        sprintf(file_name,"50-NotFailed-%02d.in",case_num);
+        int length=rnd.next(MIN_N,20);
+        string s;
+        rep(i,length){
+            int num=rnd.next(1,100);
+            if(num%10||s.back()=='x')s.push_back('o');
+            else s.push_back('x');
+        }
+        output(s,length,file_name);
+    }
+    rnd.setSeed(time(0)+getpid());
+    /*ランダムテストケース*/
+    rep(case_num,TEST_CASE){
+        char file_name[100];
+        sprintf(file_name,"60-random-%02d.in",case_num);
+        int length=rnd.next(MIN_N,MAX_N);
+        string s;
+        rep(i,length){
+            int num=rnd.next(rangemin,rangemax);
+            if(num%10)s.push_back('o');
+            else s.push_back('x');
+        }
+        output(s,length,file_name);
+    }
+    /*corner*/
+    rep(case_num,TEST_CASE){
+        char file_name[100];
+        sprintf(file_name,"60-corner-%02d.in",case_num);
+        int length=MAX_N;
+        string s;
+        rep(i,length){
+            int num=rnd.next(rangemin,rangemax);
+            if(i<=length-3)s.push_back('o');
+            else s.push_back('x');
+        }
+        output(s,length,file_name);
+    }
 }
